350baa85c0bc4e2b5dee414b1eb7491c0823838d
kira-0521 <kiratanaka0521@gmail.com>
2022-04-04 21:56:33 +0900

    expressdevサーバーランニング

--- package_learning/vite-project/backend/src/index.ts
+++ package_learning/vite-project/backend/src/index.ts
@@ -1,4 +1,36 @@
-import {main} from "./semafo";
-import { promiseAll} from "./promiseAll";
+import express, { Request, Response, NextFunction} from 'express'
+import cors from 'cors'
+import helmet from 'helmet'
+import session from 'express-session'
 
-promiseAll();
+const initializeServer = async () => {
+    const app = express()
+    const PORT = 3000
+
+    // json返却
+    app.use(express.json())
+
+    // urlをエンコード
+    app.use(express.urlencoded({ extended: true }))
+
+    // cors対策
+    app.use(cors())
+
+    // セキュリティヘッダー対策
+    app.use(helmet())
+
+    app.get('/', (req: Request, res: Response) => {
+        return res.status(200).send({
+            message: 'get request successful'
+        })
+    })
+
+    app.listen(PORT, () => {
+        console.log(`dev server running at: http://localhost:${PORT}/`)
+    })
+}
+
+(async() => {
+   await initializeServer()
+})();
+
